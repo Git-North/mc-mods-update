b1922376f384cd967d61645a9fffb60defc90442
Git-North <72702125+Git-North@users.noreply.github.com>
2022-08-30 23:12:29 +0300

    Update run_me_in_the_future.bat

--- run_me_in_the_future.bat
+++ run_me_in_the_future.bat
@@ -1,3 +1,6 @@
-mklink /D "./" "%appdata%/.minecraft/mods"
+::mklink /D "./" "%appdata%/.minecraft/mods"
+
+::mkdir %date:~-10,2%.%date:~7,2%.%date:~-4,4%
+
 powershell -command curl -L "" "https://www.7-zip.org/a/7zr.exe" --output "./resources/7zr.exe"
 "./resources/7zr.exe" x *.zip

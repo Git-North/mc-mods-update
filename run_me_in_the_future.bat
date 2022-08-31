996c4cdb7b353b1ad0fab1f39fdbed8025a72aae
Git-North <72702125+Git-North@users.noreply.github.com>
2022-08-31 03:10:16 +0300

    getting closer
    7zip "Cannot open the file as archive" error

--- run_me_in_the_future.bat
+++ run_me_in_the_future.bat
@@ -1,3 +1,5 @@
+
+
 SET "psCommand="(new-object -COM 'Shell.Application')^
 .BrowseForFolder(0,'Select where Minecraft is installed',0,26).self.path""
 FOR /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "folder=%%I"
@@ -5,15 +7,25 @@ FOR /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "folder=%%I"
 setlocal enabledelayedexpansion
 set dest=!folder!\mods
 
+
+
 IF NOT EXIST !folder! (
   echo no folder selected or folder not found.
-mklink /D "./" "!folder!\mods"
+  <nul set /p "=Press any key to exit...
+  pause >nul
+  endlocal
+) else (
+curl -L https://www.7-zip.org/a/7zr.exe --output resources\7zr.exe
 
-cd !folder!
+mklink /D ".\mods" "!folder!\mods"
+
+cd !folder!\mods
 xcopy *.* %date:~-10,2%.%date:~7,2%.%date:~-4,4% /i
+
 del *.jar*
 
 cd %~dp0
-for /f "tokens=2" %%A in ('path.cmd') do curl -L "%%A" -o mods.zip
-powershell -command curl -L "" "https://www.7-zip.org/a/7zr.exe" --output "./resources/7zr.exe"
-"./resources/7zr.exe" x *.zip
+for /f "tokens=2" %%A in ('resources\path.cmd') do curl -L "%%A" -o mods.zip
+
+"%~dp0/resources/7zr.exe" x *.zip
+)

540d4024569080fa789db907945d1e09c9f7a458
Git-North (in school) <72702125+Git-North@users.noreply.github.com>
2022-11-07 14:24:58 +0300

    Temp file

--- resources/sd/path.cmd
+++ resources/sd/path.cmd
@@ -1,1 +1,2 @@
 curl -s https://api.github.com/repos/QuiltMC/quilt-native-installer/releases | findstr browser_download_url | findstr windows
+pause
--- resources/sd/test2.cmd
+++ resources/sd/test2.cmd
@@ -1,2 +1,2 @@
-for /f "tokens=2" %%A in ('path.cmd') do powershell -command -first 1 | curl -L "%%A" -O
-powershell -command -first 1
+for /f "tokens=2" %%A in ('path.cmd') do powershell -command -first 1 | echo %%A> curlthis.txt
+

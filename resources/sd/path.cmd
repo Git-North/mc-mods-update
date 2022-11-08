3f4d583b48cd8259e912b5fe21f6251e5d519fdf
Git-North (in school) <72702125+Git-North@users.noreply.github.com>
2022-11-08 12:25:23 +0300

    We can get the latest now
    just gotta curl it

--- resources/sd/path.cmd
+++ resources/sd/path.cmd
@@ -1,1 +1,1 @@
-curl -s https://api.github.com/repos/QuiltMC/quilt-native-installer/releases | findstr browser_download_url | findstr windows
+curl -k -s https://api.github.com/repos/QuiltMC/quilt-native-installer/releases | findstr browser_download_url | findstr windows
--- resources/sd/test2.cmd
+++ resources/sd/test2.cmd
@@ -1,3 +1,4 @@
-for /f "tokens=2" %%A in ('path.cmd') do echo %%A >> curlthis.txt
+for /f "tokens=2" %%A in ('path.cmd') do echo %%A >> curlthis.tmp
+powershell -command Get-Content curlthis.tmp -Head 2
 pause
 

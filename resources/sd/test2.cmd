4c5ec35939648d11cb9f9252f82e477355890031
Git-North <72702125+Git-North@users.noreply.github.com>
2022-11-08 21:09:05 +0300

    It works now merge with run file

--- resources/sd/test2.cmd
+++ resources/sd/test2.cmd
@@ -1,4 +1,7 @@
+SETLOCAL ENABLEDELAYEDEXPANSION 
 for /f "tokens=2" %%A in ('path.cmd') do echo %%A >> curlthis.tmp
-powershell -command (Get-Content -Path .\curlthis.tmp -TotalCount 2)[-1]
-pause
+powershell -command (Get-Content -Path '.\curlthis.tmp' -TotalCount 2)[-1] > latest.tmp
+for /f "Tokens=* Delims=" %%x in (latest.tmp) do curl -L %%x -o "quilt-installer.exe"
+del *.tmp
 
+ 

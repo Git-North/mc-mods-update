da0f94e7b7a24e459bf7a860e75e6580f90987fb
Git-North (in school) <72702125+Git-North@users.noreply.github.com>
2022-11-08 13:36:33 +0300

    name

--- resources/sd/test2.cmd
+++ resources/sd/test2.cmd
@@ -1,4 +1,4 @@
 for /f "tokens=2" %%A in ('path.cmd') do echo %%A >> curlthis.tmp
-powershell -command Get-Content curlthis.tmp -Head 2
+powershell -command (Get-Content -Path .\curlthis.tmp -TotalCount 2)[-1]
 pause
 

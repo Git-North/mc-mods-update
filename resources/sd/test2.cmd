243152b48555d9c21fd68adb216cdac973421c12
Git-North <72702125+Git-North@users.noreply.github.com>
2022-11-10 20:05:14 +0300

    Delete test2.cmd

--- resources/sd/test2.cmd
+++
@@ -1,7 +0,0 @@
-SETLOCAL ENABLEDELAYEDEXPANSION 
-for /f "tokens=2" %%A in ('path.cmd') do echo %%A >> curlthis.tmp
-powershell -command (Get-Content -Path '.\curlthis.tmp' -TotalCount 2)[-1] > latest.tmp
-for /f "Tokens=* Delims=" %%x in (latest.tmp) do curl -L %%x -o "quilt-installer.exe"
-del *.tmp
-
- 

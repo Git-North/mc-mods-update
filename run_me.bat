7321c5016522b8310cf9d6471591585759160174
Git-North <72702125+Git-North@users.noreply.github.com>
2023-01-01 23:14:51 +0300

    Update run_me.bat

--- run_me.bat
+++ run_me.bat
@@ -8,14 +8,13 @@ FOR /f "tokens=* delims=" %%I in (`resources\mcdir.txt`) do set "folder=%%I"
 mkdir !folder!\mods
 mkdir !folder!\.backups-of-old-mods
 set dest=!folder!\mods
-set backups=!folder!\.backups-of-old-mods
 
 
 
 mklink /D ".\mods" "!dest!"
 
 cd !folder!\mods
-xcopy *.* !backups!\%date:~-10,2%.%date:~7,2%.%date:~-4,4% /i
+xcopy *.* .disabled\%date:~-10,2%.%date:~7,2%.%date:~-4,4% /i
 
 del *.jar*
 

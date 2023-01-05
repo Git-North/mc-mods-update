e166e1c2f28db27a2aee7db4a1083aff74075b53
Git-North (school) <72702125+Git-North@users.noreply.github.com>
2023-01-05 10:31:01 +0300

    Update run_me.bat

--- run_me.bat
+++ run_me.bat
@@ -8,17 +8,26 @@ FOR /f "tokens=* delims=" %%I in (`resources\mcdir.txt`) do set "folder=%%I"
 set /p pathchoice= Would you like to choose your minecraft path rather than the default one? (%appdata%/.minecraft) [Default is 'No' which is recommended for most people]
 
 
-::IF /i '%pathchoice%' = DYULSDFCV KSDZFCVGBLFGUVLWSFEIGWIEUFGDIWFEGIWEUFIWEUFG
+IF '%pathchoice%' = '' GOTO rest1
+IF /i '%pathchoice%' = 'no' GOTO rest1
+IF /i '%pathchoice%' = 'n' GOTO rest1
+IF '%pathchoice%' = '0' GOTO rest1
+
+IF /i '%pathchoice%' = 'yes' GOTO pathcustom
+IF /i '%pathchoice%' = 'ye' GOTO pathcustom
+IF /i '%pathchoice%' = 'y' GOTO pathcustom
+IF '%pathchoice%' = '1' GOTO pathcustom
+
 
 :pathcustom
-::echo Please select your minecraft filepath
-::SET "psCommand="(new-object -COM 'Shell.Application')^
-::.BrowseForFolder(0,'Select where Minecraft is installed',0,26).self.path""
-::FOR /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "folder=%%I"
-::
-::echo "!folder!" > resources\mcdir.txt
+echo Please select your minecraft filepath
+SET "psCommand="(new-object -COM 'Shell.Application')^
+.BrowseForFolder(0,'Select where Minecraft is installed',0,26).self.path""
+FOR /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "folder=%%I"
 
-::GOTO rest1
+echo "!folder!" > resources\mcdir.txt
+
+GOTO rest1
 :rest1
 mkdir !folder!\mods
 set dest=!folder!\mods

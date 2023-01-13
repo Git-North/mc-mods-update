8aad1df357a8a2a644c06f2f977ac219075d73f5
Git-North (school) <72702125+Git-North@users.noreply.github.com>
2023-01-13 13:34:34 +0300

    Update path.cmd

--- resources/path.cmd
+++ resources/path.cmd
@@ -1,1 +1,1 @@
-curl -s https://api.github.com/repos/Git-North/mc-mods-update/releases/latest | findstr browser_download_url
+curl -k -s https://api.github.com/repos/Git-North/mc-mods-update/releases/latest | findstr browser_download_url

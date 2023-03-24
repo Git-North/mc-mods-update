a1e11fdf4cc80fed492a2159cc9c139cfc60949a
Git-North <72702125+Git-North@users.noreply.github.com>
2023-03-24 15:15:15 +0300

    Update run_me.bat

--- run_me.bat
+++ run_me.bat
@@ -82,11 +82,11 @@ GOTO rest2
 del *.zip *.7z *.rar
 xcopy *.* !dest!
 
-::cd "%~dp0\resources\"
-::for /f "tokens=2" %%B in ('quiltmc-path.cmd') do echo %%B >> curlthis.tmp
-::powershell -command (Get-Content -Path '.\curlthis.tmp' -TotalCount 2)[-1] > latest.tmp
-::for /f "Tokens=* Delims=" %%Q in (latest.tmp) do curl -L %%Q -o "quilt-installer.exe"
-::del *.tmp
+cd "%~dp0\resources\"
+for /f "tokens=2" %%B in ('quiltmc-path.cmd') do echo %%B >> curlthis.tmp
+powershell -command (Get-Content -Path '.\curlthis.tmp' -TotalCount 2)[-1] > latest.tmp
+for /f "Tokens=* Delims=" %%Q in (latest.tmp) do curl -L %%Q -o "quilt-installer.exe"
+del *.tmp
 
-::START "" "%~dp0\resources\quilt-installer.exe"
-::)
+START "" "%~dp0\resources\quilt-installer.exe"
+)

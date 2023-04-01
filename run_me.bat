8f197ba12beee9bfa49671d2c397b4e02016d9f3
Git-North <72702125+Git-North@users.noreply.github.com>
2023-04-01 14:17:52 +0300

    first commit

--- run_me.bat
+++ run_me.bat
@@ -1,27 +1,21 @@
+
 chcp 65001
 
-curl -s <https://www.7-zip.org/a/7zr.exe> -o resources/7zr.exe
+curl -s https://www.7-zip.org/a/7zr.exe -o resources/7zr.exe
 
 @echo off
 setlocal enabledelayedexpansion
 :START
+
 rem Create a new mcdir.txt file in the resources directory
 powershell -command New-Item -Path .\resources -Name "mcdir.txt"
-rem Check if the file size is 0, if it is, then set the contents to '%appdata%\.minecraft'
-for %%x in (.\resources\mcdir.txt) do if %%~zx==0 (
-    echo "%appdata%\.minecraft"> .\resources\mcdir.txt
-)
-rem Read the contents of the mcdir.txt file into the 'folder' variable
-FOR /f "tokens=* delims=" %%I in (.\resources\mcdir.txt) do set "folder=%%I"
 
-rem Set the 'javafolder' variable to the path of the java folder
-
-rem Set the 'javapath' variable to the path of the javaw.exe file
 
 rem Ask the user if they want to choose a custom Minecraft filepath or use the default one
 echo # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 set /p pathchoice=Would you like to choose your minecraft path rather than the default one? "(Default path is %appdata%\.minecraft)" [Default selection is 'No' which is recommended for most people]
 echo # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
 
 rem Check the user's response and set the 'folder' variable accordingly
 IF '%pathchoice%' == '' GOTO rest1
@@ -33,29 +27,68 @@ IF /i '%pathchoice%' == 'yes' GOTO pathcustom
 IF /i '%pathchoice%' == 'ye' GOTO pathcustom
 IF /i '%pathchoice%' == 'y' GOTO pathcustom
 IF '%pathchoice%' == '1' GOTO pathcustom
+IF '%pathchoice%' == '2' GOTO pathcustom
+
+
 
 :pathcustom
+
+for %%x in (.\resources\mcdir.txt) do if %%~zx==0 (
+    goto continue
+)
+
+
+:alreadyedited
+set /p alreadyedited=It seems that you already have already changed your Minecraft filepath. Would you like to change it again? or would you like to continue with the current filepath? [1 or 2]
+
+
+IF '%alreadyedited%' == '1' GOTO change
+
+IF '%alreadyedited%' == '2' GOTO continue
+
+:change
 rem Ask the user to select their Minecraft filepath
 echo Please select your minecraft filepath
 SET "psCommand="(new-object -COM 'Shell.Application')^
 .BrowseForFolder(0,'Select where Minecraft is installed',0,26).self.path""
 FOR /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "folder=%%I"
+GOTO continue
 
-:thisbishempty
-rem Write the 'folder' variable to the mcdir.txt file
-echo "!folder!" > resources\mcdir.txt
-rem Check if the 'folder' variable is not equal to '%appdata%', if it is, then continue to the next step, otherwise go back to the 'pathcustom' label
-if not !folder! == '%appdata%' GOTO rest1
-if !folder! == '%appdata%' GOTO pathcustom
+:continue
 
-GOTO rest1
+
+
 :rest1
+
+
+rem Check if the file size is 0, if it is, then set the contents to '%appdata%\.minecraft'
+for %%x in (.\resources\mcdir.txt) do if %%~zx==0 (
+    echo "%appdata%\.minecraft"> .\resources\mcdir.txt
+)
+rem Read the contents of the mcdir.txt file into the 'folder' variable
+FOR /f "tokens=* delims=" %%I in (.\resources\mcdir.txt) do set "folder=%%I"
+
+
 rem Create a 'mods' folder in the 'folder' directory
 mkdir !folder!\mods
 rem Set the 'dest' variable to the 'mods' folder path
 set dest=!folder!\mods
 rem Set the 'pending' variable to the 'pending_mods' folder path
 set pending=%~dp0\pending_mods
+
+
+rem Set the 'javafolder' variable to the path of the java folder
+
+rem Set the 'javapath' variable to the path of the javaw.exe file
+
+
+
+:::thisbishempty
+::rem Write the 'folder' variable to the mcdir.txt file
+::echo "!folder!" > resources\mcdir.txt
+::rem Check if the 'folder' variable is not equal to '%appdata%', if it is, then continue to the next step, otherwise go back to the 'pathcustom' label
+::if not !folder! == '%appdata%' GOTO rest1
+::if !folder! == '%appdata%' GOTO pathcustom
 
 rem Delete the 'mods' folder in the current directory
 rmdir %~dp0\mods

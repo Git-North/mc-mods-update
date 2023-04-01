d279c6e567ea0e62824e50d1cd99c6f7b185be36
Git-North <72702125+Git-North@users.noreply.github.com>
2023-04-01 13:07:59 +0300

    clean-up

--- test.bat
+++ test.bat
@@ -1,6 +1,8 @@
+@echo off
+
+
 mkdir %temp%\mc-mods-installer\
 cd "%temp%\mc-mods-installer\"
-for /f "tokens=2" %%B in ('quiltmc-path.cmd') do echo %%B >> curlthis.tmp
+for /f "tokens=2" %%B in ('quiltmc-path.cmd') do echo %%B 
 powershell -command (Get-Content -Path '.\curlthis.tmp' -TotalCount 2)[-1] > latest.tmp
 for /f "Tokens=* Delims=" %%Q in (latest.tmp) do curl -L %%Q -o "quilt-installer.exe"
-del *.tmp
